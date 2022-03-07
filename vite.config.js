e8a35c18356273f31e0532b9eeda4fce3a633d7e
Garey Simpson <garey.simpson@coinbase.com>
2022-03-07 12:26:26 -0500

    remove unused path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
-const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
